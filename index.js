8be43381af347faea0e68719db65aa7f8af68080
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-10-16 18:07:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 // Write your code here
 class Breakfast {
-  constructor(main, side, drink) {
-    this.main = main;
-    this.side = side;
+  constructor(food, drink) {
+    this.food = food;
     this.drink = drink;
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.dessert = dessert;
   }
 }
